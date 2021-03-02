5edff39ebff0366e0d022c3c8dff89cdc83d8643
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-03-02 13:25:16 -0500

    RHPAM integration

--- src/main/java/one/entropy/archiver/PamArchiver.java
+++ src/main/java/one/entropy/archiver/PamArchiver.java
@@ -4,15 +4,22 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.ContextName;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.component.properties.PropertiesComponent;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.kie.camel.KieCamelConstants.KIE_CLIENT;
+import static org.kie.camel.KieCamelConstants.KIE_OPERATION;
+import static org.kie.camel.KieCamelUtils.asCamelKieName;
 
 @ApplicationScoped
 @Startup
@@ -20,6 +27,9 @@ import java.util.stream.Collectors;
 public class PamArchiver extends RouteBuilder {
 
     private static final String IDS_HEADER = "PAM_IDs";
+
+    @Resource(mappedName = "java:jboss/DefaultJMSConnectionFactory")
+    private ConnectionFactory connectionFactory;
 
     @Inject
     @ContextName("archiver")
@@ -30,15 +40,21 @@ public class PamArchiver extends RouteBuilder {
         PropertiesComponent pc = new PropertiesComponent();
         pc.setLocation("archiver.properties");
         camelContext.addComponent("properties", pc);
+
+        JmsComponent component = new JmsComponent();
+        component.setConnectionFactory(connectionFactory);
+        camelContext.addComponent("jms", component);
     }
 
     @Override
     public void configure() throws Exception {
 
-        from("timer:archiver?period=1h&repeatCount=1")
+        from("jms:queue:archive-request")
                 .to("direct:process-archiver")
                 .to("direct:node-archiver")
-                .to("direct:variable-archiver");
+                .to("direct:variable-archiver")
+                .process(this::prepareKieResponse)
+                .toD("kie:http://{{login}}@localhost:8080/kie-server/services/rest/server");
 
         from("direct:process-archiver")
                 .to("sql:{{processes.select}}?dataSource=#mainDS")
@@ -80,5 +96,16 @@ public class PamArchiver extends RouteBuilder {
     private void setIDsToDelete(Exchange exchange) {
         List<Long> ids = exchange.getIn().getHeader(IDS_HEADER, List.class);
         exchange.getIn().setBody(ids);
+    }
+
+    private void prepareKieResponse(Exchange exchange) {
+        Map<String, Object> headers = exchange.getIn().getHeaders();
+        headers.put(KIE_CLIENT, "process");
+        headers.put(KIE_OPERATION, "signalProcessInstance");
+        headers.put(asCamelKieName("containerId"), headers.get("KIE_DeploymentId"));
+        headers.put(asCamelKieName("processInstanceId"), headers.get("KIE_ProcessInstanceId"));
+        headers.put(asCamelKieName("signalName"), "archiver-response");
+        headers.put(asCamelKieName("event"), "empty");
+        exchange.getIn().setHeaders(headers);
     }
 }
